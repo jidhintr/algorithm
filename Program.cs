df066e4cc35e444e3a82b503950bdbb123aa16d0
jidhin <jidhintr@gmail.com>
2022-09-16 22:21:47 +0200

    fwk

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var key = 7;
 var result = FindSubArraySumMatchKey(initialArray, key);
 System.Console.WriteLine(result);
 
+var len = LongestDistinctSubString("araaraci", 2);
+System.Console.WriteLine($"Distinct string length is {len}");
 
 static int FindSubArraySumMatchKey(int[] array, int key)
 {
@@ -21,4 +23,27 @@ static int FindSubArraySumMatchKey(int[] array, int key)
         }
     }
     return min;
+}
+
+
+static int LongestDistinctSubString(string data, int key)
+{   //araaci
+    var result = 0;
+    var max = 0;
+    var index = 0;
+    for (int i = 0; i <= data.Length - 1; i++)
+    {
+        var rightIndex = data[i];
+        max++; // use dict or hashmap to get count 
+        while (max > key)
+        {
+            var leftChar = data[index];
+            index++;
+            max--; // same use dict here - logic same as above func()
+        }
+    result = i -  index + 1;
+    }
+    
+    return result;
+
 }
